3f7c302db6ad69d297ece563a516e71f2b988937
Cael Foster <cfoster19@ewu.edu>
2023-01-22 00:37:00 -0800

    wow this sucks
    I cant get images, or css to work with linking, i want short relative paths so its easier to transfer

---
+++ Landing-Page-code/script.js
@@ -0,0 +1,35 @@
+// check js is connected
+console.log('hiii hiii ^_^');
+
+//i made this a while ago and ill have use for it soon//
+
+/////////////////invert button///////////////////////////
+
+/* const button = document.getElementsByClassName("button")[0];
+let toggle = false;
+button.addEventListener("click", (e) => {
+  toggle = !toggle;
+
+  const notImg = document.querySelector('*');
+  const img = document.querySelector('#img2');
+  const img2 = document.querySelector('.containerright');
+  
+
+  if (toggle) {
+    notImg.style.filter = "invert(1)";
+
+
+    /*double inverts*/
+
+    /*
+    img.style.filter = "invert(1)";
+    img2.style.filter = "invert(1)";
+  } else {
+    img.style.filter = "invert(0)";
+    img2.style.filter = "invert(0)";
+    notImg.style.filter = "invert(0)";
+  }
+}); 
+
+*/
+////////////////////////////////////////
